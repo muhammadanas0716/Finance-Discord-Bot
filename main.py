85a894524acb31e3e75797da050cc0305ad4576c
Muhammad Anas <iam.muhammadanas0716@gmail.com>
2024-06-19 15:30:02 +0400

    final today

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import discord
+from discord.ext import commands
+from discord.commands import SlashCommandGroup
 import os
 from dotenv import load_dotenv
 
@@ -9,18 +11,20 @@ load_dotenv()
 token = os.getenv('TOKEN')
 
 # Set the Intents
-intents = discord.Intents.default()
-intents.message_content = True
-
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # Console log when the bot is ready
-@client.event
+@bot.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+
+help = SlashCommandGroup(name="help", description="Help command", guild_ids=[1252923017022148679])
+
+
+
 # Respond to the message
-@client.event
+@help.command
 async def on_message(message):
     if message.author == client.user:
         return
